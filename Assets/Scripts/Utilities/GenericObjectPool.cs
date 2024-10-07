259a4401a719cad9b75af963ba9d2fefc617ddf5
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-10-07 15:40:01 +0200

    Implement ReturnItem() method in Generic Object Pool

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -5,13 +5,13 @@ namespace CosmicCuration.Utilities
 {
     public class GenericObjectPool<T> where T : class
     {
-        private List<PooledItem<T>> poolItems = new List<PooledItem<T>>();
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
 
         protected T GetItem()
         {
-            if (poolItems.Count > 0)
+            if (pooledItems.Count > 0)
             {
-                PooledItem<T> item = poolItems.Find(item => !item.isUsed);
+                PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
                 if (item != null)
                 {
                     item.isUsed = true;
@@ -27,13 +27,19 @@ namespace CosmicCuration.Utilities
             PooledItem<T> newItem = new PooledItem<T>();
             newItem.Item = CreateItem();
             newItem.isUsed = true;
-            poolItems.Add(newItem);
+            pooledItems.Add(newItem);
             return newItem.Item;
         }
 
         protected virtual T CreateItem()
         {
             throw new NotImplementedException("Child class don't have to implementation CreateItem");
+        }
+
+        public void ReturnItem(T item)
+        {
+            PooledItem<T> pooledItem = pooledItems.Find(item => item.Item.Equals(item));
+            pooledItem.isUsed = false;
         }
 
         public class PooledItem<T>
